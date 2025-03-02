78f8d8ee2d02805a424d74863b1ed6058ae5f37c
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2025-03-02 07:45:32 -0700

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 import userServices from "../services/userServices.js";
 
-// POST route, used with signUp.html&.js
+// POST route, used with signUp.html & .js for registering users
 const registerUser = async (req, res) => {
     const { username, firstName, lastName, email, password, userRole } = req.body;
 
@@ -21,6 +21,37 @@ const registerUser = async (req, res) => {
     }
 };
 
-export default{
-    registerUser
-}
+// POST route, used for logging in users
+const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    // Validate input
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+
+    try {
+        
+        const user = await userServices.validateUserLogin(username, password);
+
+        if (user) {
+            
+            res.status(200).json({
+                message: "Login successful",
+                user: { id: user.id, username: user.username, role: user.role_id }
+            });
+        } else {
+            // Invalid username or password
+            res.status(400).json({ message: "Invalid username or password" });
+        }
+
+    } catch (err) {
+        console.error("Error during login:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export default {
+    registerUser,
+    loginUser  
+};
