fc084b295eabefb707640140af3461c717d2c084
Austin <hoelscher.austin@yahoo.com>
2025-03-07 23:56:46 -0600

    All CRUD functions added to appointmentController

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -2,17 +2,95 @@ import appointmentServices from "../services/appointmentServices.js";
 
 const getAppointment = async (req, res) => {
 
+    // Grab appointment id from url
+    const appointment_id = req.params.id;
+
+    try{
+
+        const appointment = await appointmentServices.getAppointment(appointment_id);
+
+        // Check if appointment was found
+        if(appointment.length === 0){
+            return res.status(404).json({message: "Appointment Not Found."});
+        }
+        return res.status(200).json(appointment);
+    }
+    catch(err){
+        console.error("Error While Fetching Appointment: ", err);
+        return res.status(500).json({message: "Internal Server Error"});
+    }
 };
 
 const addAppointment = async (req, res) => {
 
+    // Grab user ID from url
+    const user_id = req.params.user_id;
+
+    // name of required fields
+    const requiredFields = ["app_date", "purpose"];
+
+    // Check if all reqiured fields are filled
+    if(!requiredFields.every(field => req.body[field])){
+        return res.status(400).json({message: "Missing Required Fields."});
+    }
+
+    try{
+        await appointmentServices.addAppointment(...Object.values(req.body),user_id);
+        return res.status(201).json({message: "Appointment Created Successfully!"});
+    }
+    catch(err){
+        console.error("Error When Adding Appointment: ", err);
+        return res.status(500).json({message: "Internal Server Error."});
+    }
 };
 
 const updateAppointment = async (req, res) => {
 
+    // Grab appointment ID from url
+    const appointment_id = req.params.id;
+
+    // Array for checking all required fields
+    const requiredFields = ["app_date", "purpose"];
+
+    //check if all required fields are filled
+    if(!requiredFields.every(field => req.body[field])){
+        return res.status(400).json({message: "Missing Required Fields."});
+    }
+
+    try{
+        const updatedApp = await appointmentServices.updateAppointment(...Object.values(req.body), appointment_id);
+
+        // Check if any appointments were updated
+        if(updatedApp.affectedRows === 0){
+            return res.status(404).json({message: "Appointment Not Found."});
+        }
+
+        return res.status(200).json({message: "Appointment Updated Successfully!"});
+
+    }
+    catch(err){
+        console.log("Error When Updating Appointment: ", err);
+        return res.status(500).json({message: "Internal Server Error."});
+    }
 };
 
 const deleteAppointment = async (req, res) => {
+    // Get appointment ID from url
+    const appointment_id = req.params.id;
+
+    try{
+        const delAppointment = await appointmentServices.deleteAppointment(appointment_id);
+
+        if(delAppointment.affectedRows === 0){
+            return res.status(404).json({message: "Appointment Not Found."});
+        }
+
+        return res.status(200).json({message: "Appointment Deleted Successfully!"});
+    }
+    catch(err){
+        console.error("Error When Deleting Appointment: ", err);
+        return res.status(500).json({message: "Internal Server Error."});
+    }
 
 };
 
