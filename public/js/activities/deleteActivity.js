e08173f1012fc3f8d580cc665a01ce15efd3743a
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2025-03-23 09:01:13 -0600

    Create deleteActivity.js

---
+++ public/js/activities/deleteActivity.js
@@ -0,0 +1,18 @@
+function deleteActivity(activityId) {
+    if (!confirm("Are you sure you want to delete this activity?")) return;
+
+    fetch(`http://localhost:5000/api/activities/${activityId}`, {
+        method: "DELETE",
+    })
+    .then(res => res.json())
+    .then(data => {
+        alert(data.message);
+
+    
+        location.reload(); 
+    })
+    .catch(err => {
+        console.error("Error deleting activity:", err);
+        alert("Failed to delete activity.");
+    });
+}
