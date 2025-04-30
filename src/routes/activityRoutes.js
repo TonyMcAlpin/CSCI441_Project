d580bf024cedfa9811c79b4ace1192e898c98ef2
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2025-04-30 14:21:52 -0600

    added get route for average

--- src/routes/activityRoutes.js
+++ src/routes/activityRoutes.js
@@ -4,8 +4,15 @@ import activityController from "../controllers/activityController.js"
 const router = express.Router();
 
 router.get("/:id", activityController.getActivity);
+router.get("/:user_id/average", activityController.getAverageActivity);  
+
+
+
 router.post("/:user_id", activityController.addActivity);
+
+
 router.put("/:id", activityController.updateActivity);
+
 router.delete("/:id", activityController.deleteActivity);
 
 
