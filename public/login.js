398c2a3385233f5bbf857f63d13b74b843ae34f7
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2025-05-09 07:32:32 -0600

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,3 +1,8 @@
+/* written by: Anthony McAlpin 
+tested by: Anthony McAlpin 
+debugged by: Anthony McAlpin  */
+
+
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault(); // Prevent form submission
 
