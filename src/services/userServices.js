b9407d7c907ae95ed1d1ede38d3cbb583145cb00
TonyMcAlpin <145806172+TonyMcAlpin@users.noreply.github.com>
2025-05-09 07:35:34 -0600

    Update userServices.js

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,6 @@
-/* written by:  
-tested by: Isaac Nevarez-Saenz
-debugged by: Isaac Nevarez-Saenz */
+/* written by: Austin Hoelscher, Anthony McAlpin 
+tested by: Austin Hoelscher, Anthony McAlpin, Isaac Nevarez-Saenz
+debugged by: Austin Hoelscher, Anthony McAlpin, Isaac Nevarez-Saenz  */
 
 import db from "../connections/db.js";
 import bcrypt from "bcryptjs"
